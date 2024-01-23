a08c55e92e99aeae32291b7cae8b781bbf271686
Oleksandr.Zoria <oleksandr.zoria@p2h.com>
2024-01-23 15:24:35 +0200

    first commit

---
+++ src/main/java/com/expenses/walletwatch/WalletWatchApplication.java
@@ -0,0 +1,13 @@
+package com.expenses.walletwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletWatchApplication.class, args);
+	}
+
+}
