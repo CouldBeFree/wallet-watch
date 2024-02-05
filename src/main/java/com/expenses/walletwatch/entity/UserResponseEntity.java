e0a3b860a54914e0ef6d1fea89cc72a4af0a6037
Rostyslav Stryzhak <rostyslav.stryzhak@cd.p2h.com>
2024-02-05 16:16:57 +0200

    feat: jwt changes 2

--- src/main/java/com/expenses/walletwatch/entity/UserResponseEntity.java
+++ src/main/java/com/expenses/walletwatch/entity/UserResponseEntity.java
@@ -1,11 +1,9 @@
 package com.expenses.walletwatch.entity;
 
-import com.expenses.walletwatch.utils.UserValidator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
