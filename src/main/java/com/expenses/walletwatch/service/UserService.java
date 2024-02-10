fdea53cfb8adee11c3b8cc9efc774e7e238f06a1
couldBefree <zoriaalexandr@gmail.com>
2024-02-10 11:57:35 +0200

    Added endpoint for get user

--- src/main/java/com/expenses/walletwatch/service/UserService.java
+++ src/main/java/com/expenses/walletwatch/service/UserService.java
@@ -2,18 +2,36 @@ package com.expenses.walletwatch.service;
 
 import com.expenses.walletwatch.dao.UserDao;
 import com.expenses.walletwatch.dto.UserRegistrationDto;
+import com.expenses.walletwatch.dto.UserResponseDto;
 import com.expenses.walletwatch.entity.UserEntity;
 import com.expenses.walletwatch.exception.BadRequest;
 import com.expenses.walletwatch.mapper.UserMapper;
+import com.expenses.walletwatch.utils.GetUserData;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserDao userDao;
+    private final GetUserData getUserData;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, GetUserData getUserData) {
         this.userDao = userDao;
+        this.getUserData = getUserData;
+    }
+
+    public UserResponseDto getLoggedInUser() {
+        Long userId = getUserData.getUserIdFromToken();
+        try {
+            UserEntity user = userDao.getUserById(userId);
+            return new UserResponseDto(
+                    user.getEmail(),
+                    user.getUsername(),
+                    user.getId()
+            );
+        } catch (RuntimeException e) {
+            throw new BadRequest(e.getMessage());
+        }
     }
 
     public UserRegistrationDto registerUser(UserEntity userEntity) {
